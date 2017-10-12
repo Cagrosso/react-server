a3272a6e717d515ca0e68947d7ee5cffed1ba3a6
Cameron <cameron.panagrosso@gmail.com>
2017-10-11 20:56:44 -0400

    Add react files

---
+++ web/js/clock.js
@@ -0,0 +1,35 @@
+var options = {
+    weekday: "long", year: "numeric", month: "short",
+    day: "numeric", hour: "2-digit", minute: "2-digit",
+    second: "2-digit"
+};
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.startClock();
+        this.state = {
+            currentTime: new Date().toLocaleDateString("en-us", options)
+        };
+    }
+
+    startClock() {
+        setInterval(() => {
+            this.setState({
+                currentTime: new Date().toLocaleDateString("en-us", options)
+            });
+        }, 1000);
+    }
+
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "h1",
+                null,
+                this.state.currentTime
+            )
+        );
+    }
+}
---
+++ web/jsx/clock.jsx
@@ -0,0 +1,31 @@
+var options = {  
+    weekday: "long", year: "numeric", month: "short",  
+    day: "numeric", hour: "2-digit", minute: "2-digit",
+    second: "2-digit"  
+}; 
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.startClock();
+        this.state = {
+            currentTime: (new Date()).toLocaleDateString("en-us", options)
+        };
+    }
+
+    startClock() {
+        setInterval(()=>{
+            this.setState({
+                currentTime: (new Date()).toLocaleDateString("en-us", options)
+            })
+        }, 1000);
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>{this.state.currentTime}</h1>
+        </div>
+        );
+    }
+}
