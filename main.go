3e269d953c8b8c47321deb9b31252736ee33e3e3
Cameron <cameron.panagrosso@gmail.com>
2017-10-12 19:41:16 -0400

    Fix static file serving

--- main.go
+++ main.go
@@ -3,29 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
+	"regexp"
 
 	"github.com/gorilla/mux"
 )
 
-var (
-	// Path to the Project
-	ProjectPath string
-)
-
-func init() {
-	ProjectPath = os.Getenv("PROJECT_PATH")
-}
-
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", serveHome).Methods("GET")
+	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("./web/js"))))
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println("Hit '/' endpoint")
-	http.FileServer(http.Dir(filepath.Join(ProjectPath, "web", "index.html")))
+	extension, _ := regexp.MatchString("\\.+[a-zA-Z]+", r.URL.EscapedPath())
+	// If the url contains an extension, use file server
+	if extension {
+		http.FileServer(http.Dir("./web/js")).ServeHTTP(w, r)
+	} else {
+		http.ServeFile(w, r, "./web/index.html")
+	}
 }
